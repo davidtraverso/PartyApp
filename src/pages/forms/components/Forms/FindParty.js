b1a6e5528427638e303fe05fede16de733f5cb9a
davidtraverso <davidtraverso88@gmail.com>
2019-09-25 22:48:49 -0500

    rendered select

--- src/pages/forms/components/Forms/FindParty.js
+++ src/pages/forms/components/Forms/FindParty.js
@@ -4,7 +4,9 @@ class FindParty extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      parties: []
+      parties: [],
+      selectedParty: {},
+      hideButton: true
     };
   }
 
@@ -29,11 +31,31 @@ class FindParty extends Component {
     getRequest();
   }
 
+  // Select a party
+  handleSelect = e => {
+    e.preventDefault();
+    this.setState({
+      selectedParty: this.state.parties[e.target.key],
+      hideButton: false
+    });
+  };
   render() {
     return (
       <div>
         <div id="paper" className="container p-5 text-center shadow-lg">
           <h1>FindParty</h1>
+          <select onChange={this.handleSelect}>
+            {this.state.parties.map((party, i) => {
+              return (
+                <option key={i} value={party.party_name}>
+                  {party.party_name}
+                </option>
+              );
+            })}
+          </select>
+          <br />
+          <br />
+          <input type="button" value="Join this Party!" hidden={this.state.hideButton} />
         </div>
       </div>
     );
